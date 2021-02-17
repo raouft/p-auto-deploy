2d139c724f65d840a2bbc84e06095e9cf4e16dda
Raouf Tiliouine <hello@raouft.ca>
2021-02-16 19:22:34 -0700

    deploy feb 16

--- work-template.php
+++ work-template.php
@@ -6,7 +6,7 @@ get_header();?>
     <?php $args = array(
         'post_type' => 'project',
         'order' => 'ASC',
-        'posts_per_page' => -1
+        'posts_per_page' => 8
       );
       $query = new WP_Query( $args );
       ?>
@@ -26,7 +26,13 @@ get_header();?>
           </div>
         </a>
       </div>
-      <?php endwhile; else: ?>
+      <?php endwhile; ?>
+
+      <div class="post-nav mt-4">
+        <?php previous_posts_link('<svg width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M-1-1h582v402H-1z"/><g><path fill="#13989e" d="M10.1 22.995a1 1 0 000-1.42l-4.6-4.6h23.54a1 1 0 100-2H5.52l4.58-4.57a1 1 0 000-1.41 1 1 0 00-1.41 0l-6.36 6.36a.88.88 0 000 1.27l6.35 6.37a1 1 0 001.42 0z"/></g></svg>'); ?>
+        <?php next_posts_link('<svg width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M-1-1h582v402H-1z"/><g><path d="M22 9a1 1 0 000 1.42l4.6 4.6H3.06a1 1 0 100 2h23.52L22 21.59A1 1 0 0022 23a1 1 0 001.41 0l6.36-6.36a.88.88 0 000-1.27L23.42 9A1 1 0 0022 9z" fill="#13989e"/></g></svg>', $query->max_num_pages); ?>
+      </div>
+      <?php else: ?>
         <?php get_template_part( 'content', 'none' ); ?>
           <?php wp_reset_postdata(); ?>
         <?php endif;?>
